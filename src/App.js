bf4fc695819ed9002d2ec1c5575ee9939d0762aa
julian Medellin <jdmedellinm2@gmail.com>
2022-10-25 12:51:25 -0500

    commit del flaco gonorriento

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Flaco gonorriento ya casi empezamos con toda !!
         </p>
         <a
           className="App-link"
